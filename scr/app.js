0acdded3e17a7beef54996b57a6a93841a8844a0
Kaue <kauedpadilha06@gmail.com>
2026-09-17 23:23:49 -0300

    Estratura basica funcionando

--- scr/app.js
+++ scr/app.js
@@ -1,3 +1,19 @@
-console.log(process.env.PORT);
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+
+
+app.listen(port, () =>{
+    console.log('sevidor rodando na porta: ',port);
+});
+
+
+app.post('/eco',(req,res) =>{
+    
+    console.log(req.body.text);
+    res.json({'resposta' : req.body.text});
+
+});
 
 
